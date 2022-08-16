92ee68a01d600d0777e833298d0d554ab677e61c
lorenradis <lorenradis@gmail.com>
2022-08-16 15:35:20 -0700

    Add files via upload

---
+++ Assets/Scripts/Managers/RegionManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RegionManager  {
+
+    public Region desertRegion;
+    public Region forestRegion;
+    public Region lakeRegion;
+
+    public List<Region> regions = new List<Region>();
+
+    public RegionManager()
+    {
+
+    }
+
+    public void SetupRegions()
+    {
+        desertRegion = new Region("Desert", AffinityManager.fireAffinity);
+        forestRegion = new Region("Forest", AffinityManager.grassAffinity);
+        lakeRegion = new Region("Lake", AffinityManager.waterAffinity);
+
+        regions.Add(forestRegion);
+        regions.Add(lakeRegion);
+        regions.Add(desertRegion);
+    }
+
+}
