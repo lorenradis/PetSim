d220a402e049fb39e5c3763f7132082640d9f483
lorenradis <lorenradis@gmail.com>
2022-08-19 14:54:38 -0700

    Update InteractablePet.cs

--- Assets/Scripts/InteractablePet.cs
+++ Assets/Scripts/InteractablePet.cs
@@ -6,11 +6,27 @@ public class InteractablePet : Interactable
 {
     private PetInfo petInfo;
 
+    public Dialog interactDialog;
+
+    private void Start()
+    {
+        interactDialog = new Dialog(
+            "Your pet " + petInfo.petName + " looks happy!  What would you like to do?","", null, false, "Check", "Assign", "More", "Cancel");
+    }
+
     public override void OnInteract()
     {
         base.OnInteract();
 
-        GameManager.instance.ShowPetInfo(petInfo);
+        DialogManager.instance.ShowDialog(interactDialog, () =>{
+            GameManager.instance.ShowPetInfo(petInfo);
+        }, () => {
+            //bring up the assign task menu
+        }, () => {
+            //show whatever the hell we end up putting here
+        }, () => {
+            //hide the prompt    
+        });
     }
 
     public void SetPetInfo(PetInfo newPet)
