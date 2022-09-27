d12f51d46e7cb35801607bcaa3f7f3ee88431c3c
lorenradis <lorenradis@gmail.com>
2022-09-27 14:21:16 -0700

    Update LootDrop.cs

--- Assets/Scripts/LootDrop.cs
+++ Assets/Scripts/LootDrop.cs
@@ -10,9 +10,77 @@ public class LootDrop : MonoBehaviour
     public List<Item> rareDrops = new List<Item>();   //30%
     public List<Item> epicDrops = new List<Item>();   //10%
 
+    [SerializeField] private int alwaysWoodCount;
+    [SerializeField] private int alwaysWaterCount;
+    [SerializeField] private int alwaysSandCount;
+
+    [SerializeField] private int alwaysBerriesCount;
+    [SerializeField] private int alwaysMushroomsCount;
+    [SerializeField] private int alwaysSucculentsCount;
+
+    [SerializeField] private int commonWoodCount;
+    [SerializeField] private int commonWaterCount;
+    [SerializeField] private int commonSandCount;
+
+    [SerializeField] private int commonBerriesCount;
+    [SerializeField] private int commonMushroomsCount;
+    [SerializeField] private int commonSucculentsCount;
+
+    [SerializeField] private int rareWoodCount;
+    [SerializeField] private int rareWaterCount;
+    [SerializeField] private int rareSandCount;
+
+    [SerializeField] private int rareBerriesCount;
+    [SerializeField] private int rareMushroomsCount;
+    [SerializeField] private int rareSucculentsCount;
+
+    [SerializeField] private int epicWoodCount;
+    [SerializeField] private int epicWaterCount;
+    [SerializeField] private int epicSandCount;
+
+    [SerializeField] private int epicBerriesCount;
+    [SerializeField] private int epicMushroomsCount;
+    [SerializeField] private int epicSucculentsCount;
+
     public int lootChance = 75;
 
     public GameObject dropObjectPrefab;
+
+    private void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if(i < alwaysWoodCount) alwaysDrops.Add(ItemManager.wood);
+            if(i < commonWoodCount) commonDrops.Add(ItemManager.wood);
+            if(i < rareWoodCount) rareDrops.Add(ItemManager.wood);
+            if(i < epicWoodCount) epicDrops.Add(ItemManager.wood);
+
+            if(i < alwaysWaterCount) alwaysDrops.Add(ItemManager.water);
+            if(i < commonWaterCount) commonDrops.Add(ItemManager.water);
+            if(i < rareWaterCount) rareDrops.Add(ItemManager.water);
+            if(i < epicWaterCount) epicDrops.Add(ItemManager.water);
+            
+            if(i < alwaysSandCount) alwaysDrops.Add(ItemManager.sand);
+            if(i < commonSandCount) commonDrops.Add(ItemManager.sand);
+            if(i < rareSandCount) rareDrops.Add(ItemManager.sand);
+            if(i < epicSandCount) epicDrops.Add(ItemManager.sand);
+            
+            if(i < alwaysBerriesCount) alwaysDrops.Add(ItemManager.berries);
+            if(i < commonBerriesCount) commonDrops.Add(ItemManager.berries);
+            if(i < rareBerriesCount) rareDrops.Add(ItemManager.berries);
+            if(i < epicBerriesCount) epicDrops.Add(ItemManager.berries);
+            
+            if(i < alwaysMushroomsCount) alwaysDrops.Add(ItemManager.mushrooms);
+            if(i < commonMushroomsCount) commonDrops.Add(ItemManager.mushrooms);
+            if(i < rareMushroomsCount) rareDrops.Add(ItemManager.mushrooms);
+            if(i < epicMushroomsCount) epicDrops.Add(ItemManager.mushrooms);
+            
+            if(i < alwaysSucculentsCount) alwaysDrops.Add(ItemManager.succulent);
+            if(i < commonSucculentsCount) commonDrops.Add(ItemManager.succulent);
+            if(i < rareSucculentsCount) rareDrops.Add(ItemManager.succulent);
+            if(i < epicSucculentsCount) epicDrops.Add(ItemManager.succulent);
+        }
+    }
 
     public void DropLoot()
     {
@@ -36,7 +104,6 @@ public class LootDrop : MonoBehaviour
                 {
                     newLoot.GetComponent<DroppedLoot>().SetItem(commonDrops[Random.Range(0, commonDrops.Count)]);
                 }
-                //drop common
             }
             else if (roll < 90)
             {
@@ -44,18 +111,13 @@ public class LootDrop : MonoBehaviour
                 {
                     newLoot.GetComponent<DroppedLoot>().SetItem(rareDrops[Random.Range(0, rareDrops.Count)]);
                 }
-                //drop rare
             }
             else
             {
-                //drop epic
                 if (epicDrops.Count > 0)
                 {
-
                     newLoot.GetComponent<DroppedLoot>().SetItem(epicDrops[Random.Range(0, epicDrops.Count)]);
                 }
-
-                
             }
         }
     }
